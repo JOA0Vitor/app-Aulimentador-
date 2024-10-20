3d99a894d30366c840a11db6f45b2df14a64a5c3
Ian Lohan <ianlohan.acc@gmail.com>
2024-10-20 18:37:57 -0300

    V1 para apresentacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
 import 'package:aulimentador/controller.dart';
 import 'package:aulimentador/global_style.dart';
 import 'package:aulimentador/route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
